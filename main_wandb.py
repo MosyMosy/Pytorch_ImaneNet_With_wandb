d33c9d8cf89ec467b8f4e8c36bfbf68434c4c0d8
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2022-01-15 23:03:23 +0330

    workers

--- main_wandb.py
+++ main_wandb.py
@@ -41,8 +41,8 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                     help='model architecture: ' +
                     ' | '.join(model_names) +
                     ' (default: resnet18)')
-parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
-                    help='number of data loading workers (default: 1)')
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers (default: 8)')
 parser.add_argument('--epochs', default=3, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
